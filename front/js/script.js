87c245c03ecc24e8c2e1909555f61712616ada86
mdmirkhalili <majb.ms@gmail.com>
2023-04-30 21:10:18 +0330

    Add unit test

--- front/js/script.js
+++ front/js/script.js
@@ -1,1 +1,12 @@
 console.log('its worked')
+
+export function myFunction (a , b){
+ let output ; 
+ if(typeof a === 'string' || typeof b === 'string'){
+  output = a+' '+b
+ } else if(typeof a === 'number' && typeof b === 'number') {
+  output = a+b ;
+ }
+
+ return output ;
+}
---
+++ front/js/script.spec.js
@@ -0,0 +1,13 @@
+import { myFunction } from "./script"
+
+describe('test my function' ,()=>{
+  
+  it('should be sum number' , ()=>{
+    const t = myFunction(2,6);
+    expect(t).toBe(8)
+  })
+  it('should be sum string' , ()=>{
+    const l = myFunction('ali' , 'mirkhalili')
+    expect(l).toBe('ali mirkhalili');
+  })
+})
